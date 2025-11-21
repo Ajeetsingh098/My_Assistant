22c4bbb79a8f218c6b102237dc7f7130071b6097
AJEET SINGH <ajeet9517singh@gmail.com>
2025-11-21 17:45:37 +0530

    Update auth.controller.js

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -69,8 +69,12 @@ export const login = async (req, res) => {
 
 export const logOut = async (req, res) => {
    try {
-    res.clearCookie("token")
-    return res.status(200).json({message:"log Out successfully"})
+  res.clearCookie('token', { 
+    httpOnly: true,
+    secure: true, /
+    sameSite: 'none' 
+  });
+  res.status(200).json({ message: "Logged out successfully" });
    } catch (error) {
     return res.status(500).json({message:`logOut error ${error}`})
    }
