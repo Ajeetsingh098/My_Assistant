7aac9eda35ed348a97bd2dc322b90c22b449ff88
AJEET SINGH <ajeet9517singh@gmail.com>
2025-11-22 16:57:04 +0530

    Update user.routes.js

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -5,9 +5,10 @@ import isAuth from "../middlewares/isAuth.js";
 import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router()
+userRouter.get("/youtube/search", searchYoutube);
 
 userRouter.get('/current', isAuth, getCurrentUser)
 userRouter.post('/update', isAuth, upload.single('assistantImage'), updateAssistant)
 userRouter.post("/asktoassistant",isAuth,askToAssistant)
-userRouter.get("/youtube/search",isAuth, searchYoutube);
+
 export default userRouter
